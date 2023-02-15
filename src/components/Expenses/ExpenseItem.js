d3b95319afd507079c5a38b4325f7cd372a4b96e
Davidperezz <dperez@hermescorp.io>
2023-02-14 19:11:56 -0600

    carpetas, botones, cambios de valor renderizados en DOM

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,25 @@
+import ExpenseDate from './ExpenseDate';
+import Card from '../UI/Cards';
+import { useState } from 'react';
+import "./ExpenseItem.css";
+function ExpenseItem(props) {
+
+  const [title, newTitle] = useState(props.title);
+
+  const botonTitleHandler = () =>{
+    newTitle('Updated!');
+    console.log(title);
+  }
+
+  return (
+    <Card className="expense-item">
+      <ExpenseDate date={props.date}/>
+      <div className="expemse-item__description">
+        <h2>{title}</h2>
+        <div className="expense-item__price">${props.amount}</div>
+      </div>
+      <button onClick={botonTitleHandler}>Change Title</button>
+    </Card>
+  );
+}
+export default ExpenseItem;
